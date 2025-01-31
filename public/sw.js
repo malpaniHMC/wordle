b14db24aee24c3a06131ea7eb947822ff291ef68
malpaniHMC <malpani.shivam@gmail.com>
2025-01-30 19:11:02 -0800

    6 letter words

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 //! IF ANYTHING IN THIS FILE IS CHANGED MAKE SURE setVersion.js HAS ALSO BEEN UPDATED
 // Increase the v number when the app is updated
-const version = "1.5.2";
+const version = "1.5.4";
 const cacheName = `wordle+-v${version}`;
 
 const assetsToCache = [
